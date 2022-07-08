d7e4245a25261370902dfef6fa5b40b89e8a38d9
かる <karuhi.dev@gmail.com>
2022-07-08 19:20:50 +0900

    fix: 常に環境変数の値を使うように変更

--- src/events/Initial.ts
+++ src/events/Initial.ts
@@ -4,19 +4,14 @@ const FULL_CHANNEL_COUNT = 5
 // 初期チャンネル設定を行う
 export async function setIniatialChannelSetting() {
   console.log('⚒️ [チャンネル] 初期設定')
-  const channels = await getChannels()
-  if (channels.length !== FULL_CHANNEL_COUNT) {
-    console.log('⚡[チャンネル] 初期化中')
-    const channels = [
-      process.env.CHID_TP,
-      process.env.CHID_A,
-      process.env.CHID_B,
-      process.env.CHID_C,
-      process.env.CHID_D
-    ]
-    for (let i = 0; i < channels.length; i++) {
-      await addChannel(i, channels[i] || '')
-    }
-    console.log('✅ [チャンネル] 初期化完了')
-  } else console.log('✅ [チャンネル] 初期設定済')
+  const channels = [
+    process.env.CHID_TP,
+    process.env.CHID_A,
+    process.env.CHID_B,
+    process.env.CHID_C,
+    process.env.CHID_D
+  ]
+  for (let i = 0; i < channels.length; i++) {
+    await addChannel(i, channels[i] || '')
+  }
 }
